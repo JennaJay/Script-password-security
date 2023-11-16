f04100c6a62c333a37a3ecb5637c3eab29da7fae
Jenna Jay <jenneh83@gmail.com>
2023-11-16 03:04:39 -0600

    Added array selectors to customer prompts

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var NumberofCharacters = 22;
-var UpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var LowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var Symbols = ['!','@','#','$','%','^','&','*','(',')','=','+','?','-','/','<','>','[',']','{','}' ]
-var Numbers = ['0','1','2','3','4','5','6','7','8','9']
-
+var userSelections = [];
+var numberofCharacters = 22;
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var symbols = ['!','@','#','$','%','^','&','*','(',')','=','+','?','-','/','<','>','[',']','{','}' ];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
 
 function generatePassword() {
-prompt('How many characters would you like your password to have?')
 
 }
 
@@ -27,10 +26,25 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function customPrompts() {
-
+  numberofCharacters = Prompt("How many characters would you like your password to have?");
+  if(numberofCharacters < 8 || numberofCharacters > 128) {
+  alert('Password must contain between 8 and 128 characters');
+  return false;
 }
 
-prompt('Password will contain Uppercase letters');
-prompt('Password will contain Lowercase letters');
-prompt('Password will contain Numbers');
-prompt('Password will contain Symbols');
+if (confirm('I want uppercase letters in my password')); {
+  userSelections = userSelections.concat(upperCase);
+}
+
+if (confirm('I want lowercase letters in my password')); {
+  userSelections = userSelections.concat(lowerCase);
+}
+
+if (confirm('I want symbols in my password')); {
+  userSelections = userSelections.concat(symbols);
+}
+
+if (confirm('I want numbers in my password')); {
+  userSelections = userSelections.concat(numbers);
+}
+}
